59b222eb6fdd5afc5951d1a0d846c931ed233c72
Bindx <487479@gmail.com>
2014-09-22 15:10:21 +0800

    Initial Commit

---
+++ ddddddd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ddddddd
+//
+//  Created by 公司 on 9/22/14.
+//  Copyright (c) 2014 Bindx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ddddddd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ddddddd
+//
+//  Created by 公司 on 9/22/14.
+//  Copyright (c) 2014 Bindx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
